5722c26cfed0400e9348d53479a18529bbd6a789
Ktabibian <Kyle.tabibian@yahoo.com>
2022-09-28 23:52:19 -0400

    coded

---
+++ src/main/java/chapter10/Cat.java
@@ -0,0 +1,12 @@
+package chapter10;
+
+public class Cat extends Animal{
+
+    public void makeSound(){
+        System.out.println("meow");
+    }
+
+    public void scratch(){
+        System.out.println("Iam a cat. I scratch things.");
+    }
+}
